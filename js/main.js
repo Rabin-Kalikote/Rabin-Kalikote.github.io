33cad06cee17f19f005f8fc49346518067be420f
Rabin Kalikote <rabinkalikote33@gmail.com>
2020-09-24 01:40:14 +0545

    Added PWA features.

--- js/main.js
+++ js/main.js
@@ -9,6 +9,13 @@ $(".hamburger-button").click( function(){
 	$(".nav").toggleClass("opened");
 });
 
+// PWA features
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('/sw.js')
+    .then(reg => console.log('service worker registered'))
+    .catch(err => console.log('service worker not registered', err));
+}
+
 // Home Slider
 var slideIndex = 0;
 showSlides();
